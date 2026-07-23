e52764d7725c4501918a619edee5caa3ea091436
Mohamed Taleb <mtaleb.contact@gmail.com>
2026-07-23 19:04:34 +0100

    add CGI tester EXECUTABLE

--- HTTP/HttpHandler.cpp
+++ HTTP/HttpHandler.cpp
@@ -160,7 +160,7 @@ bool HttpHandler::resolveDirectory(RouteMatch &match, const HttpRequest request,
 
 HttpResult HttpHandler::process(const HttpRequest &request) const
 {
-    std::cout << "[REQUEST]: HTTP/1.1 " << request.getMethod() << request.getRequestPath() << std::endl;
+    std::cout << "[REQUEST]: HTTP/1.1 " << request.getMethod() << " " << request.getRequestPath() << std::endl;
     RouteMatch match;
     resolveRoute(request, match);
     std::cout << "[MAP TO]: " << match.fullPath << std::endl;
